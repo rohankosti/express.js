570ada5ece6766d7b6257d6f00ad7d8f7c7f2b8e
rohankosti <kostirohan186@gmail.com>
2025-12-16 18:28:47 +0530

    router with crud

---
+++ router/jobpost.js
@@ -0,0 +1,61 @@
+import mongodb,{MongoClient,ObjectId} from 'mongodb';
+import { Router  } from 'express';
+import dotenv from 'dotenv';    
+dotenv.config();
+const router = Router();
+
+const client = new MongoClient(process.env.MONGO_URL);
+const dbs_name = process.env.MONGO_DB;
+
+client.connect().then(() => 
+{
+console.log("MongoDB connected successfully");
+})
+.catch((err) => 
+{
+console.log("MongoDb Server Not Connected ! Error", err);
+});
+
+const dbs = client.db(dbs_name);
+
+
+router.post('/storeJobVacancy',async (req, res) => {
+   const jobdata = req.body;
+  //  console.log(jobdata);
+   const collection = await dbs.collection('jobvecancy').insertOne(jobdata);
+   res.status(200).send({message:"Job Vacancy Posted Successfully"});
+});
+
+router.get('/jobvecancylist',async(req,res)=>{
+  const getjobpost =await dbs.collection('jobvecancy').find({}).toArray();
+  res.status(200).send(getjobpost);
+})
+
+router.post('/jobvecancysingledata',async (req,res)=>{
+    const bodyid = req.body.id;
+    const singledata = await dbs.collection('jobvecancy').findOne({_id:new ObjectId(bodyid)});
+    res.status(200).send(singledata);
+})
+router.put('/updatejobapplicationdata',async (req,res)=>{
+  const bodydata = req.body;
+  // console.log(bodydata);
+  
+  const updatedcollection = await dbs.collection('jobvecancy').updateOne({_id:new ObjectId(bodydata.id)},{
+    $set:{
+     name:bodydata.name,
+     email:bodydata.email,
+     position:bodydata.position,
+     resume:bodydata.resume
+    }
+  });
+  res.status(200).send({message:"Job Vacancy Updated Successfully"});
+});
+
+router.delete('/deletejobapplicationdata',async(req,res)=>{
+  const bodydata = req.body;
+  const deletecollection = await dbs.collection('jobvecancy').deleteOne({_id:new ObjectId(bodydata.id)});
+  res.status(200).send({message:"Job Vacancy Deleted Successfully"});
+});
+
+
+export default router;
